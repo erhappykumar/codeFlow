043a8d5106d4b3547b63ff5d20d994e187cee9cf
happy kumar <happy@gmail.com>
2025-10-17 12:51:41 +0530

    git commited

---
+++ data.js
@@ -0,0 +1,29 @@
+const artical_left_html =`<div class="artical_left_"><div class="artical_left_">
+  <a class="artical_left_a" href="#">HTML Anchor tag</a>
+</div>
+  <div class="artical_left_">
+  <a class="artical_left_a" href="#">HTML Pragraph tag</a>
+</div></div>`;
+const artical_left_css = `
+<div class="artical_left_">
+<div class="artical_left_">
+<a class="artical_left_a" href="#">CSS color</a>
+</div>
+  <div class="artical_left_">
+  <a class="artical_left_a" href="#">CSS backgroundColor</a>
+</div></div>`
+const artical_left_javascript = `    
+
+    <div class="artical_left_">
+
+<div class="artical_left_">
+  <a class="artical_left_a" href="/JS/JS Syntax.html">JS syntax</a>
+</div>
+  <div class="artical_left_">
+  <a class="artical_left_a" href="#">JS Variable</a>
+</div></div>`;
+
+const a_l_h=artical_left_html;
+const a_l_c=artical_left_css;
+const a_l_j=artical_left_javascript;
+export { artical_left_html as a_l_h, artical_left_css as a_l_c, artical_left_javascript as a_l_j }
---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+import * as p  from "./data.js"
+
+
+let body=document.querySelector("body");
+
+body.innerHTML+=`<nav><div class="tb nav_div_equal">☰</div><div class="nav_div_html">html</div><div class="nav_div_css" >css</div>
+  <div>javascript</div></nav>`;
+  body.innerHTML+=`<article class="artical_left"></article>`;
+  
+  
+  
+  const nav_div_html=document.querySelector(".nav_div_html");
+  const nav_div_css=document.querySelector(".nav_div_css");
+  
+  const nav_div_javascript=document.querySelector(".nav_div_javascript");
+  const nav_div_equal=document.querySelector(".nav_div_equal");
+  const artical_left=document.querySelector(".artical_left");
+  
+  
+const nav=document.querySelector("nav");
+
+
+  nav.addEventListener("click",(e)=>{
+    
+    
+    e.target.style.color=`green`
+    nav_div_equal.addEventListener("click",()=>{
+      
+   if(e.target.innerText==`html`){
+  artical_left.innerHTML=`${p.a_l_h}`;} 
+  else if(e.target.innerText==`css`){
+ artical_left.innerHTML=`${p.a_l_c}`;}
+else if(e.target.innerText==`javascript`)
+{artical_left.innerHTML=`${p.a_l_j}`;}
+  })////nav_div_equal
+  })//nav
+  
+
+  nav_div_equal.addEventListener("click",()=>{artical_left.classList.toggle("artical_left_toggle");
+  console.log(artical_left.className);
+  
+ })
+  
