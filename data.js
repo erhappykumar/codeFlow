d2ae5eec6bb9b6395460423bef7e6e453e22a396
happy kumar <happy@gmail.com>
2025-10-19 08:25:19 +0530

    new commited

--- data.js
+++ data.js
@@ -1,29 +1,110 @@
-const artical_left_html =`<div class="artical_left_"><div class="artical_left_">
-  <a class="artical_left_a" href="#">HTML Anchor tag</a>
-</div>
-  <div class="artical_left_">
-  <a class="artical_left_a" href="#">HTML Pragraph tag</a>
-</div></div>`;
-const artical_left_css = `
-<div class="artical_left_">
-<div class="artical_left_">
-<a class="artical_left_a" href="#">CSS color</a>
-</div>
-  <div class="artical_left_">
-  <a class="artical_left_a" href="#">CSS backgroundColor</a>
-</div></div>`
-const artical_left_javascript = `    
+   
+const article_left_html_array = [
+  "Introduction",
+  "HTML Basic Structure", 
+  "HTML Elements",
+  "Text Formatting",
+  "Links & Anchors",
+  "Images & Multimedia",
+  "Lists",
+  "Tables",
+  "Forms & Input",
+  "Semantic HTML",
+  "HTML Entities & Symbols",
+  "Inline Frames",
+  "HTML5 APIs & Advanced Features",
+  "Global Attributes & Data- Attributes",
+  "Best Practices",
+  "HTML & CSS/JS Integration",
+  "Deprecated & Obsolete Tags",
+  "References & Resources"
+];
+const article_left_css_array = [
+  "Introduction to CSS",
+  "Selectors & Combinators",
+  "Colors & Units",
+  "Typography & Fonts",
+  "Box Model",
+  "Backgrounds & Borders",
+  "Margin, Padding, and Spacing",
+  "Positioning & Display",
+  "Flexbox",
+  "Grid Layout",
+  "Float & Clear",
+  "Responsive Design & Media Queries",
+  "Transitions & Animations",
+  "Transforms",
+  "Custom Properties (CSS Variables)",
+  "Pseudo-classes & Pseudo-elements",
+  "Specificity & Inheritance",
+  "Shadows & Effects",
+  "Browser Prefixes & Compatibility",
+  "Best Practices & Optimization",
+  "Advanced CSS (Filters, Masks, Clip-Path)",
+  "CSS Functions (calc(), var(), etc.)",
+  "Print Styles",
+  "Debugging CSS",
+  "References & Resources"
+];
+const article_left_javascript_array= [
+  "Introduction",
+  "Basics & Syntax", 
+  "Strings",
+  "Numbers & Math",
+  "Arrays",
+  "Objects",
+  "Functions",
+  "Control Flow",
+  "Scope & Hoisting",
+  "Error Handling",
+  "Events (Browser)",
+  "DOM Manipulation",
+  "BOM (Browser Object Model)",
+  "JSON",
+  "Asynchronous JavaScript",
+  "Modules",
+  "Regular Expressions",
+  "Advanced Topics",
+  "Web APIs & Fetch",
+  "New ES Features (ES6+)",
+  "Debugging & Tools"
+];
 
-    <div class="artical_left_">
+let article_left_html;
+article_left_html_array.forEach((headline,i)=>{ article_left_html+= `
+  <div class="article_left_">
+    <div class="article_left_">
+      <a class="article_left_a" href="/HTML/${headline}.html">${headline}</a>
+    </div>
+  </div>`;
 
-<div class="artical_left_">
-  <a class="artical_left_a" href="/JS/JS Syntax.html">JS syntax</a>
-</div>
-  <div class="artical_left_">
-  <a class="artical_left_a" href="#">JS Variable</a>
-</div></div>`;
+ })
 
-const a_l_h=artical_left_html;
-const a_l_c=artical_left_css;
-const a_l_j=artical_left_javascript;
-export { artical_left_html as a_l_h, artical_left_css as a_l_c, artical_left_javascript as a_l_j }
+
+ 
+
+let article_left_css;
+article_left_css_array.forEach((headline) => {
+  article_left_css += `
+<div class="article_left_">
+  <div class="article_left_">
+    <a class="article_left_a" href="/CSS/${headline}.html">${headline}</a>
+  </div>
+</div>`;
+});
+let article_left_javascript;
+article_left_javascript_array.forEach((headline, index) => {
+  article_left_javascript += `
+<div class="article_left_">
+  <div class="article_left_">
+    <a class="article_left_a" href="/JS/${headline}.html">${headline}</a>
+  </div>
+</div>`;
+});
+ 
+
+const a_l_h=article_left_html;
+const a_l_c=article_left_css;
+const a_l_j=article_left_javascript;
+ export { article_left_html as a_l_h, article_left_css as a_l_c, article_left_javascript as a_l_j }
+ 
--- script.js
+++ script.js
@@ -4,9 +4,9 @@ import * as p  from "./data.js"
 
 let body=document.querySelector("body");
 
-body.innerHTML+=`<nav><div class="tb nav_div_equal">☰</div><div class="nav_div_html">html</div><div class="nav_div_css" >css</div>
+body.innerHTML+=`<nav class="nav"><div class="tb nav_div_equal">☰</div><div class="nav_div_html">html</div><div class="nav_div_css" >css</div>
   <div>javascript</div></nav>`;
-  body.innerHTML+=`<article class="artical_left"></article>`;
+  body.innerHTML+=`<article class="article_left"></article>`;
   
   
   
@@ -14,11 +14,12 @@ body.innerHTML+=`<nav><div class="tb nav_div_equal">☰</div><div class="nav_div
   const nav_div_css=document.querySelector(".nav_div_css");
   
   const nav_div_javascript=document.querySelector(".nav_div_javascript");
+  
   const nav_div_equal=document.querySelector(".nav_div_equal");
-  const artical_left=document.querySelector(".artical_left");
+  const article_left=document.querySelector(".article_left");
   
   
-const nav=document.querySelector("nav");
+const nav=document.querySelector(".nav");
 
 
   nav.addEventListener("click",(e)=>{
@@ -28,17 +29,17 @@ const nav=document.querySelector("nav");
     nav_div_equal.addEventListener("click",()=>{
       
    if(e.target.innerText==`html`){
-  artical_left.innerHTML=`${p.a_l_h}`;} 
+  article_left.innerHTML=`${p.a_l_h}`;} 
   else if(e.target.innerText==`css`){
- artical_left.innerHTML=`${p.a_l_c}`;}
+ article_left.innerHTML=`${p.a_l_c}`;}
 else if(e.target.innerText==`javascript`)
-{artical_left.innerHTML=`${p.a_l_j}`;}
+{article_left.innerHTML=`${p.a_l_j}`;}  
   })////nav_div_equal
   })//nav
   
 
-  nav_div_equal.addEventListener("click",()=>{artical_left.classList.toggle("artical_left_toggle");
-  console.log(artical_left.className);
+  nav_div_equal.addEventListener("click",()=>{article_left.classList.toggle("article_left_toggle");
+  console.log(article_left.className);
   
  })
   
